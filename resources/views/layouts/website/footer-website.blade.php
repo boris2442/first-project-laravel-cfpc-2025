f70bf82d5f4de742c3331cc50523f6f265b6a3b5
boris2442 <aubinborissimotsebo@gmail.com>
2025-06-09 15:03:42 +0200

    debug

--- resources/views/layouts/website/footer-website.blade.php
+++ resources/views/layouts/website/footer-website.blade.php
@@ -1,5 +1,5 @@
-<div class="">
-    <footer class="bg-gray-800 text-white py-4">
+<div class="w-full min-h-screen bg-gray-100 dark:bg-gray-900 text-gray-800 dark:text-gray-200 shadow-lg">
+    <footer class="bg-gray-800 rounded-[50px]  text-white py-4 ">
         <div class="container mx-auto text-center">
             <p class="text-sm">© {{ date('Y') }} Your Company Name. All rights reserved.</p>
             <p class="text-xs">Powered by Laravel and Tailwind CSS</p>
